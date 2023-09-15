1d2e3a681e672cbb2ed6147175d38325a6ed97a4
moises.gobila <12902689+gobila@users.noreply.github.com>
2023-09-14 21:23:59 -0300

    Iniciando o projeto

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const consign = require('consign');
+
+const app = express();
+
+
+consign().include('models').then('middlewares.js').then('routes').then('boot.js').into(app)
