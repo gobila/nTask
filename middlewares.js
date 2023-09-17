ceb13b00654512fc080da3b7b6c364cd17492a30
moises.gobila <12902689+gobila@users.noreply.github.com>
2023-09-16 23:30:55 -0300

    fim do capitulo 7

--- middlewares.js
+++ middlewares.js
@@ -1,5 +1,13 @@
+const bodyParser = require('body-parser')
+
 module.exports =app=>{
     
     app.set('port', 3000);
     app.set('json spaces', 4);
+    app.use(bodyParser.json());
+    app.use((req,res, next)=>{
+            // middlewere de pré execução das rotas
+            delete req.body.id;
+            next()
+        })
 }
